9097ae3b34b3e87f9abc5ea9faec6e6c7ec54d95
aaraymond <aaraymond@me.com>
2017-09-18 14:29:04 -0400

    Initial
    test

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Aaron Raymond</title>
+</head>
+
+<body>
+	<header>
+		<h1>Welcome - Please select your computer wizard.</h1>
+	</header>
+
+</body>
+</html>
---
+++ portfolio_aaron.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Aaron Raymond</title>
+</head>
+
+<body>
+	<header>
+		<h1>Aaron Raymond</h1>
+	</header>
+
+	<!-- Main Work Experience -->
+	<h2>Work Experience</h2>
+
+	<h3>Web Design - Cheyanna's Champions 4 Children</h3>
+
+		<p>I designed multiple web pages using Wordpress templates in conjuction with custom HTML and CSS. I integrated the front-end design with back-end database software and PHP forms.</p>
+
+		<ul>
+			<li>HTML</li>
+			<li>CSS</li>
+			<li>PHP</li>
+			<li>Photoshop</li>
+			<li>Lightroom</li>
+			<li>test</li>
+		</ul>
+
+	<h3>Photography - University of Rochester</h3>
+		
+		<p>I captured photos for the University of Rochester Athletic Department website and publications.</p>
+
+		<ul>
+			<li>Professional Event/Wedding Coverage</li>
+			<li>Portraits</li>
+			<li>Landscapes</li>
+		</ul>
+
+	<h3>IT Consultant - Dallas Computer Solutions (DCS)</h3>
+	
+		<p>Working at DCS expanded my knowledge in communications and problem solving through the installation and management of hundreds of computers. Managing the computer network for a large commercial real estate company was my primary task and in doing so I gained valuable experience in the corporate setting including setting up and ensuring conference rooms were prepared in advance of meetings and all computers were connected and fully prepared for presentations. My talents also included managing customer’s Macs in conjunction with Windows Small Business Server.</p>
+
+	<h2>Education</h2>
+		
+		<p>Anticipated Class of 2018 University of Rochester with a BA in Environmental Studies, minor in Computer Science, and a minor in Studio Arts.</p>
+
+
+	<h2>Extracurriculars</h2>
+		
+		<p>I founded the UR Photography Club in 2015 to promote the learning of, creating, and sharing of the art of photography. Through my leadership of the Photo Club, I have transformed the photography community at the University of Rochester into a collaborative organization providing photography services to the campus community. In addition to professional services, Photo Club also holds a trip each semester. Last semester we visited Niagara Falls and held a full day walking photo expedition around the falls starting in the United States and crossing the border into Canada. This international expedition took an incredible amount of planning but resulted in a unique learning experience coordinating such a significant trip.</p>
+
+</body>
+</html>
