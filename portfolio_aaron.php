767756c6a867505f5ba358a1b89c5caa2a96a3c5
aaraymond <aaraymond@me.com>
2017-09-18 15:04:14 -0400

    updated structure
    added section tags and about me section

--- portfolio_aaron.php
+++ portfolio_aaron.php
@@ -10,44 +10,63 @@
 		<h1>Aaron Raymond</h1>
 	</header>
 
+	<!-- About Me Section -->
+	<section>
+		<h2>About Me</h2>
+
+		<p>I am currently a senior studying Environmental Studies at the University of Rochester. I enjoy combining my photographic skills with my computer skills to create stunning websites and user experiences. I also have experience in the IT world which has helped strengthen my communication and problem solving skills.</p>
+	</section>
+
+
 	<!-- Main Work Experience -->
-	<h2>Work Experience</h2>
+	<main>
+		<h2>Work Experience</h2>
 
-	<h3>Web Design - Cheyanna's Champions 4 Children</h3>
+		<section>
+		<h3>Web Design - Cheyanna's Champions 4 Children</h3>
 
-		<p>I designed multiple web pages using Wordpress templates in conjuction with custom HTML and CSS. I integrated the front-end design with back-end database software and PHP forms.</p>
+			<p>I designed multiple web pages using Wordpress templates in conjuction with custom HTML and CSS. I integrated the front-end design with back-end database software and PHP forms.</p>
 
-		<ul>
-			<li>HTML</li>
-			<li>CSS</li>
-			<li>PHP</li>
-			<li>Photoshop</li>
-			<li>Lightroom</li>
-			<li>test</li>
-		</ul>
+			<ul>
+				<li>HTML</li>
+				<li>CSS</li>
+				<li>PHP</li>
+				<li>Photoshop</li>
+				<li>Lightroom</li>
+				<li>test</li>
+			</ul>
+		</section>
 
-	<h3>Photography - University of Rochester</h3>
-		
-		<p>I captured photos for the University of Rochester Athletic Department website and publications.</p>
+		<section>
+			<h3>Photography - University of Rochester</h3>
+				
+				<p>I captured photos for the University of Rochester Athletic Department website and publications.</p>
 
-		<ul>
-			<li>Professional Event/Wedding Coverage</li>
-			<li>Portraits</li>
-			<li>Landscapes</li>
-		</ul>
+				<ul>
+					<li>Professional Event/Wedding Coverage</li>
+					<li>Portraits</li>
+					<li>Landscapes</li>
+				</ul>
+		</section>
 
-	<h3>IT Consultant - Dallas Computer Solutions (DCS)</h3>
-	
-		<p>Working at DCS expanded my knowledge in communications and problem solving through the installation and management of hundreds of computers. Managing the computer network for a large commercial real estate company was my primary task and in doing so I gained valuable experience in the corporate setting including setting up and ensuring conference rooms were prepared in advance of meetings and all computers were connected and fully prepared for presentations. My talents also included managing customer’s Macs in conjunction with Windows Small Business Server.</p>
+		<section>
+			<h3>IT Consultant - Dallas Computer Solutions (DCS)</h3>
 
-	<h2>Education</h2>
-		
-		<p>Anticipated Class of 2018 University of Rochester with a BA in Environmental Studies, minor in Computer Science, and a minor in Studio Arts.</p>
+			<p>Working at DCS expanded my knowledge in communications and problem solving through the installation and management of hundreds of computers. Managing the computer network for a large commercial real estate company was my primary task and in doing so I gained valuable experience in the corporate setting including setting up and ensuring conference rooms were prepared in advance of meetings and all computers were connected and fully prepared for presentations. My talents also included managing customer’s Macs in conjunction with Windows Small Business Server.</p>
+		</section>
+	</main>		
 
+	<!-- My Extracurriculars -->
+	<section>
+		<h2>Extracurriculars</h2>
+			
+			<p>I founded the UR Photography Club in 2015 to promote the learning of, creating, and sharing of the art of photography. Through my leadership of the Photo Club, I have transformed the photography community at the University of Rochester into a collaborative organization providing photography services to the campus community. In addition to professional services, Photo Club also holds a trip each semester. Last semester we visited Niagara Falls and held a full day walking photo expedition around the falls starting in the United States and crossing the border into Canada. This international expedition took an incredible amount of planning but resulted in a unique learning experience coordinating such a significant trip.</p>
+	</section>		
 
-	<h2>Extracurriculars</h2>
-		
-		<p>I founded the UR Photography Club in 2015 to promote the learning of, creating, and sharing of the art of photography. Through my leadership of the Photo Club, I have transformed the photography community at the University of Rochester into a collaborative organization providing photography services to the campus community. In addition to professional services, Photo Club also holds a trip each semester. Last semester we visited Niagara Falls and held a full day walking photo expedition around the falls starting in the United States and crossing the border into Canada. This international expedition took an incredible amount of planning but resulted in a unique learning experience coordinating such a significant trip.</p>
+		<!-- Copyright footer -->
+	<footer>
+		<div>© Aaron Raymond 2017</div>
+	</footer>
 
 </body>
 </html>
