6ba859231e112ae461c12cad260d35d96d67d665
Ali Hashim <ahashim@u.rochester.edu>
2017-09-21 11:15:33 -0400

    Restructured Matt's HTML and added Lorem Ipsum

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title>CSC174, Hashim and Raymond Portfolios</title>
+	<title>CSC174, Portfolio Websites</title>
 	<link rel="stylesheet" type="text/css" href="css/stylesheet.css">
 </head>
 
--- portfolio_matt.php
+++ portfolio_matt.php
@@ -1,41 +1,65 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <title>Matthew Hood’s BIO</title>
-  <meta name="Generator" content="Cocoa HTML Writer">
-  <meta name="CocoaVersion" content="1504.83">
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+  <title>Matthew Hood</title>
  
 </head>
 
 <body>
 
-<article>
+<div class="container">
+<header>
+	<h1>Matthew Hood</h1>
+	<?php include "navigation.inc";?>
+</header>
 
- <h1>Mattew Hood's BIO!</h1>
-      
-<p>I am A Senior in my final semester at UofR majoring in Computer Science</p>
-      
-<p>Love Web Development and iPhone app Development</p>
-      
-<p>Made projects like google maps in Java and used Unreal Engine to craft my own Video Games!</p>
-      
-<p>Favorite Class was Video Game Journalism with Proof Joe!</p>
-      
-</article>
-    
-<aside>
-<h2> small facts </h2>
-    <p>UofR has been a great experience!</p>
-    
-</aside>
+<section class="about">
+	<h2>About Me</h2>
+	<p>I am a senior in my final semester at UofR majoring in Computer Science. I love Web Development and iPhone app Development. I've made projects like google maps in Java and used Unreal Engine to craft my own Video Games!</p>
+</section>
+
+<main>
+	<h2>Experiences</h2>
+
+	<section class="first">
+	<h3>Unreal Engine</h3>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget consequat ante. In rhoncus nunc ac imperdiet posuere. Morbi pharetra, ipsum non viverra lacinia, dolor magna fermentum tellus, in maximus magna dolor nec nisl. Vivamus semper fermentum metus, sed euismod felis placerat a. Pellentesque nulla nibh, ornare ac tincidunt at, ornare vitae justo. Suspendisse non dui at lectus rutrum suscipit. Vestibulum faucibus ante eu nunc gravida, sit amet tincidunt ipsum imperdiet. Pellentesque accumsan ornare tortor, sit amet iaculis arcu finibus id. Nulla mollis ultricies rutrum. Mauris ut sem purus. Duis laoreet, sapien sollicitudin bibendum porttitor, velit orci vulputate dui, at interdum leo ante eu tellus. Fusce at dictum nulla. Mauris sit amet finibus massa. Quisque ut blandit nisl, non tincidunt urna.</p> 
+
+	</section>
+
+	<section class="second">
+	<h3>Java Google Maps</h3>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget consequat ante. In rhoncus nunc ac imperdiet posuere. Morbi pharetra, ipsum non viverra lacinia, dolor magna fermentum tellus, in maximus magna dolor nec nisl. Vivamus semper fermentum metus, sed euismod felis placerat a. Pellentesque nulla nibh, ornare ac tincidunt at, ornare vitae justo. Suspendisse non dui at lectus rutrum suscipit.</p> 
+	
+	</section>
+
+	<section class="third">
+	<h3>Java Google Maps</h3>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget consequat ante. In rhoncus nunc ac imperdiet posuere. Morbi pharetra, ipsum non viverra lacinia, dolor magna fermentum tellus, in maximus magna dolor nec nisl. Vivamus semper fermentum metus, sed euismod felis placerat a. Pellentesque nulla nibh, ornare ac tincidunt at, ornare vitae justo. Suspendisse non dui at lectus rutrum suscipit.</p> 
+	
+	</section>
+
+	    
+	<section>
+	<h3>Skills</h3>
+	    <ul>
+	    	<li>Java</li>
+	    	<li>Unreal Engine</li>
+	    	<li>Swift</li>
+	    	<li>CSS</li>
+	    	<li>HTML</li>
+	    	<li>JavaScript</li>
+	    </ul>
+	</section>
+</main>
     
 <footer>
-
-<h2> THE END </h2>
-    <p> End of My BIO</p>
+<div>© Matthew Hood 2017</div>
     
 </footer>
+</div><!-- container -->
     
 </body>
 
